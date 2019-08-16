ab0f11d5165d0e2d6d2cc20758238607dffe28d1
Jabal Logian <jaballogianrma77@gmail.com>
2019-08-16 22:42:24 +0700

    adding splash screen

---
+++ app/src/main/java/com/example/wasav/ActivitySplashScreen.java
@@ -0,0 +1,34 @@
+package com.example.wasav;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class ActivitySplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMED_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+
+                Intent toActivityMain = new Intent(ActivitySplashScreen.this, ActivityMain.class);
+                toActivityMain.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(toActivityMain);
+                finish();
+            }
+        },SPLASH_TIMED_OUT);
+    }
+}
